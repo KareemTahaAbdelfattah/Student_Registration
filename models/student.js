12b8107355012f8b9188a290d02ab85967ffeacb
reem-mahmoud16 <66190640+reem-mahmoud16@users.noreply.github.com>
2024-03-24 01:41:15 +0200

    Add files via upload

---
+++ models/student.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const model = mongoose.model;
+
+const studentScheme = Schema({
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+    },
+    groupid: {
+        type: Number, // Changed to Number
+    },
+    phoneNumber: {
+        type: String,
+    },
+    birthDate: {
+        type: String,
+    },
+    gender: {
+        type: String,
+    },
+    email: {
+        type: String,
+    },
+    password: {
+        type: String,
+    },
+    profilePhoto: {
+        type: Image,
+    }
+    
+});
+
+const student = model('student', studentScheme);
+
+module.exports = {
+    student,
+};

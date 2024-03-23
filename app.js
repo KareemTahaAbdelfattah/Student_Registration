ac1589e306dbff794b6fa86ff07174c6fd96538d
Aya-abdo27 <aya195244@feng.bu.edu.eg>
2024-03-23 17:06:49 +0200

    adding new branch with courses table

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
-const express = require ("express");
-const joi = require('joi');
+const express = require("express");
 const app = express();
 app.use(express.json());
 
-const imageController = require('./controllers/image_controller');
+const { courseRouter } = require('./routes/course_router');
 
-app.get('/api/images', imageController.getAllImages);
-app.get('/api/images/:id', imageController.getImageByID);
-app.post('/api/images', imageController.addImage);
-app.put('/api/images/:id', imageController.editImageByID);
-app.delete('/api/images/:id', imageController.deleteImageByID);
+
+app.use('/api/courses/', courseRouter);
+
 
 module.exports = {
     app,

fa2f4894ad3f09a67f122faa857c1a1deb4a963a
Kareem Taha <82422224+KareemTahaAbdelfattah@users.noreply.github.com>
2024-03-23 16:31:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
-const express = require ("express");
-const joi = require('joi');
-const app = express();
-app.use(express.json());
-
-const imageController = require('./controllers/image_controller');
-
-app.get('/api/images', imageController.getAllImages);
-app.get('/api/images/:id', imageController.getImageByID);
-app.post('/api/images', imageController.addImage);
-app.put('/api/images/:id', imageController.editImageByID);
-app.delete('/api/images/:id', imageController.deleteImageByID);
+const { app }= require('./app');
 
 const port = process.env.PORT || 3000;
 app.listen(port,() => console.log("listening on port ->  " + port + '.......!'));

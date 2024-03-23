dc6eeeef9315dd170507665d588f16de6e50afe9
Kareem Taha <82422224+KareemTahaAbdelfattah@users.noreply.github.com>
2024-03-23 17:14:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-const mongoose = require("mongoose");
-const { app } = require("./app.js");
+const { app }= require('./app');
 
 const port = process.env.PORT || 3000;
-
-app.listen(port, async () => {
-    await mongoose.connect('mongodb+srv://mostafa:DOp2RNfBD4CKyvzc@cluster0.vjpftqg.mongodb.net/test2');
-    console.log(`listening on port ${port} ...`)
-});
+app.listen(port,() => console.log("listening on port ->  " + port + '.......!'));

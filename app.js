439c2c0eb9b8903ba8e4b62b2205f0ffaf0533e1
mohammed sherif <muhammed195812@feng.bu.edu.eg>
2024-03-27 01:58:28 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ app.use(express.json());
 
 const { courseRouter } = require('./routes/course_router');
 const {imageRouter} = require('./routes/image_route');
+const {studyGroupRouter} = require('./routes/studyGroup_route');
 
 app.use('/api/courses/', courseRouter);
 app.use('/api/images/', imageRouter);
+app.use('/api/studyGroups/', studyGroupRouter);
+
 
 module.exports = {
     app,

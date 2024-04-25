971802e97fb90bfaa586480e8b6eb580c8e2770d
Kareem Taha <82422224+KareemTahaAbdelfattah@users.noreply.github.com>
2024-04-25 14:15:37 +0200

    Mongoose update

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const { app }= require('./app');
+const mongoose = require('mongoose');
 
 const port = process.env.PORT || 3000;
-app.listen(port,() => console.log("listening on port ->  " + port + '.......!'));
+app.listen(port, async () => {
+    await mongoose.connect("mongodb+srv://kimotaha52:y3skXTydcmQ1wW2H@cluster0.64mgxgh.mongodb.net/");
+    console.log('listening on port ->  ' + port + '  ........');
+});

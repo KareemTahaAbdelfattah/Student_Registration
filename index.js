0bb3d12fe55deba77a9503c8611a890d82341a93
Mariam ElMonyer <64711347+mariam-elmonyer@users.noreply.github.com>
2024-04-29 01:23:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const { app }= require('./app');
 const mongoose= require("mongoose");
 const port = process.env.PORT || 3000;
 app.listen(port,() =>{
-  await mongoose.connect("");
+  await mongoose.connect('mongodb+srv://mostafa:DOp2RNfBD4CKyvzc@cluster0.vjpftqg.mongodb.net/test2')
   console.log("listening on port ->  " + port + '.......!'));

87eb1929ba8ebc36c95d0b330b1817edef85cc09
mohammed sherif <muhammed195812@feng.bu.edu.eg>
2024-05-01 19:20:13 +0300

    Update study_group.js

--- models/study_group.js
+++ models/study_group.js
@@ -1,9 +1,17 @@
-const studyGroups = [
-    {groupId: 1, cohortName: "preparatory", cohortNumber: 1},
-    {groupId: 2, cohortName: "1st", cohortNumber: 2},
-    {groupId: 3, cohortName: "2nd", cohortNumber: 3},
-    {groupId: 4, cohortName: "3nd", cohortNumber: 4},
-    {groupId: 5, cohortName: "4th", cohortNumber: 5}
-];
 
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const model = mongoose.model;
+
+const courseScheme = Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    number: {
+        type: Number,
+    },
+});
+
+const Course = model('Course', courseScheme);
 module.exports = { studyGroups, };

e470f315181b5acf5f48a6c5a4974fa2ee836634
Kareem Taha <82422224+KareemTahaAbdelfattah@users.noreply.github.com>
2024-05-02 11:34:02 +0300

    Delete models/image.js

--- models/image.js
+++
@@ -1,9 +0,0 @@
-const images = [
-    {id: 1, name: "Web RoadMap"},
-    {id: 2, name: "BackEnd RoadMap"},
-    {id: 3, name: "Frontend RoadMap"},
-    {id: 4, name: "Embedded RoadMap"},
-    {id: 5, name: "OS RoadMap"}
-];
-
-module.exports = { images, };

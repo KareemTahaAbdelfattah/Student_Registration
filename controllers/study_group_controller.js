543a50ba14bb3a0f174f6df16e48934063ebdcc7
mohammed sherif <muhammed195812@feng.bu.edu.eg>
2024-05-02 17:28:19 +0300

    Update study_group_controller.js

--- controllers/study_group_controller.js
+++ controllers/study_group_controller.js
@@ -1,147 +1,69 @@
-const joi = require('joi');
-const { studyGroups } = require('../models/studyGroup');
-const { validateStudyGroup } = require('../helpers/validation');
+const { Study_group} = require('../models/study_group');
 
-const getAllstudyGroups = async (req,res) => {
-    try{
-        const allstudyGroups = await studyGroups.find({});
-        res.status(200).send(allstudyGroups);
-    }catch (error) {
-        res.status(400).send(error);
-    }
-};
-
-const getstudyGroupsByID =  async (req,res) => {
-    try{
-        const studyGroups = await studyGroups.find({_id: req.params.id});
-        res.status(200).send(allstudyGroups);
-    }catch (error) {
-        res.status(400).send(error);
-    }
-};
-
-const addstudyGroup = async (req,res) => {
-
+const getAllstudyGroups = async (req, res) => {
     try {
-        const studyGroups = await studyGroups.create(req.body);
-        res.status(201).send(course);
+        const allstudyGroups = await Study_group.find({});
+        res.status(200).send(allstudyGroups);
     } catch (error) {
         res.status(400).send(error);
     }
 };
 
-const editstudyGroupByID = async (req,res) => {
-    try{
-        const studyGroups = await studyGroups.find({_id: req.params.id});
-        if (!studyGroups) {
-            return res.status(404).send("studyGroup not found");
-        }
+const getstudyGroupsById = async (req, res) => {
+    try {
+        
+        const StudyGroup = await Study_group.findOne({ _id: req.params.id });
+        res.status(200).send(StudyGroup);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+};
 
-        // Update existing fields
+const addstudyGroup = async (req, res) => {
+    try {
+        const StudyGroup = await Study_group.create(req.body);
+        res.status(201).send(StudyGroup);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+};
+
+const editstudyGroup = async (req, res) => {
+    const userId = req.params.id;
+    try {
+        const StudyGroup = await Study_group.findOne({ _id: userId });
+        if (!StudyGroup) {
+            return res.status(404).send("Study Group not found");
+        }
+        if (req.body.cohortname) {
+            StudyGroup.cohortname = req.body.cohortname;
+        }
+        if (req.body.cohortnumber) {
+            StudyGroup.cohortnumber = req.body.cohortnumber;
+        }
         if (req.body.groupid) {
-            course.groupid = req.body.groupid;
+            StudyGroup.groupid = req.body.groupid;
         }
-        if (req.body.name) {
-            course.name = req.body.name;
-        }
-        if (req.body.number) {
-            course.number = req.body.number;
-        }
-        await studyGroups.save();
-        res.status(200).send(course);
-
-    }catch (error) {
-        res.status(400).send(error);
-    }
-};
-
-const deletestudyGroupByID = async (req,res) => {
-    try {
-        const studyGroups = await studyGroups.deleteOne({ _id: req.params.id });
-        res.status(200).send(studyGroups);
+        await StudyGroup.save();
+        res.status(200).send(StudyGroup);
     } catch (error) {
         res.status(400).send(error);
     }
 };
 
+const deletestudyGroupbyID = async (req, res) => {
+    try {
+        const StudyGroup = await Study_group.deleteOne({ _id: req.params.id });
+        res.status(200).send(StudyGroup);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+};
 
 module.exports = {
     getAllstudyGroups,
-    getstudyGroupsByID,
+    getstudyGroupsById,
     addstudyGroup,
-    editstudyGroupByID,
-    deletestudyGroupByID,
-};const joi = require('joi');
-const { studyGroups } = require('../models/studyGroup');
-const { validateStudyGroup } = require('../helpers/validation');
-
-const getAllstudyGroups = async (req,res) => {
-    try{
-        const allstudyGroups = await studyGroups.find({});
-        res.status(200).send(allstudyGroups);
-    }catch (error) {
-        res.status(400).send(error);
-    }
-};
-
-const getstudyGroupsByID =  async (req,res) => {
-    try{
-        const studyGroups = await studyGroups.find({_id: req.params.id});
-        res.status(200).send(allstudyGroups);
-    }catch (error) {
-        res.status(400).send(error);
-    }
-};
-
-const addstudyGroup = async (req,res) => {
-
-    try {
-        const studyGroups = await studyGroups.create(req.body);
-        res.status(201).send(course);
-    } catch (error) {
-        res.status(400).send(error);
-    }
-};
-
-const editstudyGroupByID = async (req,res) => {
-    try{
-        const studyGroups = await studyGroups.find({_id: req.params.id});
-        if (!studyGroups) {
-            return res.status(404).send("studyGroup not found");
-        }
-
-        // Update existing fields
-        if (req.body.groupid) {
-            course.groupid = req.body.groupid;
-        }
-        if (req.body.name) {
-            course.name = req.body.name;
-        }
-        if (req.body.number) {
-            course.number = req.body.number;
-        }
-        await studyGroups.save();
-        res.status(200).send(course);
-
-    }catch (error) {
-        res.status(400).send(error);
-    }
-};
-
-const deletestudyGroupByID = async (req,res) => {
-    try {
-        const studyGroups = await studyGroups.deleteOne({ _id: req.params.id });
-        res.status(200).send(studyGroups);
-    } catch (error) {
-        res.status(400).send(error);
-    }
-};
-
-
-module.exports = {
-    getAllstudyGroups,
-    getstudyGroupsByID,
-    addstudyGroup,
-    editstudyGroupByID,
-    deletestudyGroupByID,
+    editstudyGroup,
+    deletestudyGroupbyID,
 };

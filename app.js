86a2d816322c3a1b0f6e17ccdd41efdec2ea5916
Kareem Taha <82422224+KareemTahaAbdelfattah@users.noreply.github.com>
2024-05-02 14:12:22 +0300

    Update app.js

--- app.js
+++ app.js
@@ -7,13 +7,14 @@ const {imageRouter} = require('./routes/image_route');
 const { courseRouter } = require('./routes/course_router');
 const {studentRouter} = require('./routes/student_router');
 const { studentWithCoursesRouter } = require('./routes/students_with_courses_router');
+const {studyGroupRouter} = require('./routes/study_group_router');
 
 
 app.use('/api/images/', imageRouter);
 app.use('/api/courses/', courseRouter);
 app.use('/api/students/', studentRouter);
 app.use('/api/studentsWithCourses/', studentWithCoursesRouter);
-
+app.use('/api/studyGroups/', studyGroupRouter);
 
 module.exports = {
     app,

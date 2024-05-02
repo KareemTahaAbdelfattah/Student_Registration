5712beae807e64c780dff82a2c95c33337df01a2
Kareem Taha <82422224+KareemTahaAbdelfattah@users.noreply.github.com>
2024-05-02 11:47:47 +0300

    Delete models/studyGroup.js

--- models/studyGroup.js
+++
@@ -1,9 +0,0 @@
-const studyGroups = [
-    {groupId: 1, cohortName: "preparatory", cohortNumber: 1},
-    {groupId: 2, cohortName: "1st", cohortNumber: 2},
-    {groupId: 3, cohortName: "2nd", cohortNumber: 3},
-    {groupId: 4, cohortName: "3nd", cohortNumber: 4},
-    {groupId: 5, cohortName: "4th", cohortNumber: 5}
-];
-
-module.exports = { studyGroups, };
